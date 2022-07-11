4e9026564fa70a3285fb0955af96897f6c43c437
lahiruv-swivel <lahiruv@swivelgroup.com.au>
2022-07-11 10:56:23 +0530

    Initial commit

---
+++ src/main/java/com/swivel/lahiru/powerhousetemp/PowerHouseTempApplication.java
@@ -0,0 +1,13 @@
+package com.swivel.lahiru.powerhousetemp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PowerHouseTempApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PowerHouseTempApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/swivel/lahiru/powerhousetemp/PowerHouseTempApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swivel.lahiru.powerhousetemp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PowerHouseTempApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
